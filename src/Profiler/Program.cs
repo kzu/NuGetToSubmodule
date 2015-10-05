4c19c29d5bbb4b84edf43334f5c3306889a98aac
Daniel Cazzulino <daniel@cazzulino.com>
2015-10-05 02:05:47 -0300

    Sample app with nugets for two dependent sdks

---
+++ src/Profiler/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Profiler
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			var sdk = new LittleSdk.Messaging.Messaging(new LittleSdk.LittleSdk());
+
+			sdk.Send ("Hello World!");
+		}
+	}
+}
